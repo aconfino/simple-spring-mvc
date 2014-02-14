005c6f7ea91d0c989bb1a35c7a0eea4336fdcd0e
unknown <aconfino@gmail.com>
2014-02-14 12:50:01 -0500

    sgd

--- deploy.bat
+++ deploy.bat
@@ -3,3 +3,8 @@ cd C:/workspace/openshift-sample-app/jbossews/webapps/
 git add .
 git commit -m "update"
 git push
+
+branch to push: master
+target remote name: origin
+
+git push -u ssh://52fd800f5004469e44000011@jbossews-darthvadar.rhcloud.com/~/git/jbossews.git master:origin
--- src/main/java/com/confino/common/controller/HelloWorldController.java
+++ src/main/java/com/confino/common/controller/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
 	public ModelAndView helloWorld() {
 
 		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "Hey Dennis...autodeployed 28 times!");
+		model.addObject("msg", "Hey Dennis...autodeployed 29 times!");
 
 		return model;
 	}
